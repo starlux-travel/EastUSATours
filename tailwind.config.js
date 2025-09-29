ceda000444998636182dbf64ae00051a8a1a515a
Nikki liu <nickeyara@gmail.com>
2025-09-29 00:51:00 -0400

    移除編譯檔 tailwind.css，避免 merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   content: [
-<<<<<<< HEAD
-    "./templates/**/*.html",   // 掃描 Django 模板
-    "./**/templates/**/*.html",// 掃描 app 裡的模板
-    "./static/**/*.js",        // 掃描靜態 JS
-=======
-    "./templates/**/*.html",                // 專案全域 templates
-    "./eastusatours/templates/**/*.html",   // 主 app
-    "./tours/templates/**/*.html",          // tours app
-    "./cruise/templates/**/*.html",         // cruise app
-    "./accounts/templates/**/*.html",       // accounts app
-    "./**/templates/**/*.html",             // 保險，全域 templates
-    "./static/src/**/*.{js,ts,css,jsx,tsx}" // 前端 js / css
->>>>>>> recover-tours
+    "./templates/**/*.html",
+    "./eastusatours/templates/**/*.html",
+    "./tours/templates/**/*.html",
+    "./cruise/templates/**/*.html",
+    "./accounts/templates/**/*.html",
+    "./static/src/**/*.{js,ts,css,jsx,tsx}",
   ],
   theme: {
     extend: {},
